a44fbfc870eb3c53977233203055bf437f91f779
Jason Wu <jason@jasondembp.pft.com>
2016-03-15 15:03:31 +0800

    first commit

---
+++ OfficeLottery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OfficeLottery
+//
+//  Created by Jason Wu on 2016/3/15.
+//  Copyright © 2016年 Jason Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
